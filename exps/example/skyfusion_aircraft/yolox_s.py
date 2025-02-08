55ac5ba858e79854d76739330e42ec94c4c353ad
natsunoyuki <user@192.168.1.10>
2025-02-08 15:53:26 +0800

    Added new files for SkyFusion aircraft filtered data.

---
+++ exps/example/skyfusion_aircraft/yolox_s.py
@@ -0,0 +1,25 @@
+# SkyFusion: Aerial Object Detection
+# A synthetic dataset generated fromAITODv2 and Airbus Aircraft Detection
+# https://www.kaggle.com/datasets/kailaspsudheer/tiny-object-detection
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 0.33
+        self.width = 0.50
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+        # Define yourself dataset path
+        self.data_dir = "datasets/SkyFusion_aircraft/"
+        self.train_ann = "instances_train2017.json"
+        self.val_ann = "instances_val2017.json"
+
+        self.num_classes = 1
+
+        self.max_epoch = 100
+        self.data_num_workers = 4
+        self.eval_interval = 1
